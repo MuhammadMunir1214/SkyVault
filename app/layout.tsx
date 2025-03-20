817fec1138d1c0988057fa6d14a6e5df2425c459
Muhammad Munir <mmunir1214@gmail.com>
2025-03-19 23:38:41 -0400

    Landing Page Items edited.

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "SkyVault",
-  description: "Sky Vault: Cloud storage, elevated!",
+  description: "Sky Vault: Cloud storage, elevated!!!",
 };
 
 export default function RootLayout({
